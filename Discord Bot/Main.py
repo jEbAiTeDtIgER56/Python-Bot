0552ae3bde11269166bf8735b576f58a2e39db54
jEbAiTeDtIgER56 <75805773+jEbAiTeDtIgER56@users.noreply.github.com>
2021-12-03 19:28:25 +0100

    deatg updates

--- Discord Bot/Main.py
+++ Discord Bot/Main.py
@@ -9,10 +9,28 @@ async def on_ready():
 
 @client.command()
 async def ping(ctx):
-    await ctx.send('uïn heh')
+    await ctx.send(f'uÃ¯n heh: {client.latency * 1000}')
+
+@client.command(brief="sends the bot's invite link")
+async def invite(ctx):
+    async with ctx.typing():
+        await ctx.send('insert invite link here')
+
+@commands.Cog.listener()
+async def on_guild_join(guild):
+    for channel in guild.text_channels:
+        if channel.permissions_for(guild.me).send_messages:
+            await channel.send('Hi')
+            break
+
+@client.event
+async def on_message(message):
+    if 'bruh' in message.clean_content.lower():
+        message.channel.send('BRUH')
+    await client.process_commands(message)
 
 @client.command()
 async def stop(ctx):
     await ctx.send('no u :c')
 
-client.run('OTEyNzMxMDA5ODM2MDIzODY4.YZ0M_g.VyGyMS5ZHajNr8CHXezvOWtEN_A')
+client.run('')
